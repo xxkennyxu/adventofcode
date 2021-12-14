ba77ec42b8775cd5e9c20650810d616a680162fb
Kenny Xu <xxkennyxu@gmail.com>
2021-12-13 16:16:39 -0800

    add visibility to function signatures

--- util/AOCProblem.java
+++ util/AOCProblem.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.List;
 
 public abstract class AOCProblem {
-    void run() throws IOException {
+    public void run() throws IOException {
         System.out.println("=== Part 1 ===");
         System.out.println(">> Test Input: " + part1(getTestInput()));
         System.out.println(">> Main Input: " + part1(getInput()));
@@ -12,11 +12,11 @@ public abstract class AOCProblem {
         System.out.println(">> Main Input: " + part2(getInput()));
     }
 
-    List<String> getInput() throws IOException {
+    private List<String> getInput() throws IOException {
         return HelperElf.getAllLinesFromInputFile(getDay(), getYear());
     }
 
-    List<String> getTestInput() throws IOException {
+    private List<String> getTestInput() throws IOException {
         return HelperElf.getAllLinesFromTestFile(getDay(), getYear());
     }
 
